4ccd739f84251c19f6b7814928ed2777db3130a8
saratb <saratbondada2014@gmail.com>
2020-09-11 15:19:06 -0400

    Correct changes

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let width = 10
   let squares = []
+  let bombAmount = 20
 
   // Create Board
   function createBoard(){
+    //get shuffled game array with random bombs
+    const bombsArray = Array(bombAmount).fill('bomb')
+    const emptyArray = Array(width*width - bombAmount).fill('valid')
+    console.log(bombsArray)
+    console.log(emptyArray)
     for(let i = 0; i < width*width; i++){
       const square = document.createElement('div')
       square.setAttribute('id', i)
