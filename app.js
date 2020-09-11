6d47b6ca6df5bd232be4322898d42db21af50d7a
saratb <saratbondada2014@gmail.com>
2020-09-11 18:25:32 -0400

    HTML: File updated with container CSS: Grid shown with bombs JS: Bombs established for game

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
   // Create Board
   function createBoard(){
     //get shuffled game array with random bombs
-    const bombsArray = Array(bombAmount).fill('bomb')
-    const emptyArray = Array(width*width - bombAmount).fill('valid')
-    console.log(bombsArray)
-    console.log(emptyArray)
+    const bombsArray = Array(bombAmount).fill('bomb') // The Bombs
+    const emptyArray = Array(width*width - bombAmount).fill('valid') //Which ones are empty
+
+    const gameArray = emptyArray.concat(bombsArray) //Result in 2 arrays being joined
+
+    const shuffledArray = gameArray.sort(() => Math.random() -0.5)
+
+
+
+
+
     for(let i = 0; i < width*width; i++){
       const square = document.createElement('div')
       square.setAttribute('id', i)
+      square.classList.add(shuffledArray[i])
       grid.appendChild(square)
       squares.push(square)
 
